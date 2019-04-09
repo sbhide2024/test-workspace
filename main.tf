82c3cdcc97ed223edb9c646521c99708d0489eb1
Paul Welch <git@pwelch.net>
2019-04-09 10:57:37 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
 # main.tf
 
-# terraform {
-#   backend "remote" {
-#     hostname     = "app.terraform.io"
-#     organization = "pwelch_staging"
-#
-#     workspaces {
-#       name = "test-workspace-random"
-#     }
-#   }
-# }
-
 resource "random_pet" "server" {
   keepers = {
     uuid = "${uuid()}"
